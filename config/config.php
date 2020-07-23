14f6fd089065fd744bc87759a6184691b1aa29ac
kausar-zaman <41296913+kausar-zaman@users.noreply.github.com>
2020-07-23 11:14:56 +0100

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define("DB_HOST", "localhost");
+define("DB_USER", "unn_w17022892");
+define("DB_PASS", "nufc2010");
+define("DB_NAME", "unn_w17022892");
+
+define("SITE_TITLE", "Empire Jobs");
